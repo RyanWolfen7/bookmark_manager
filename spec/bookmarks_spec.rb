6ea75d7bcf40a4819e373210ac5d92a5c3086f9e
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-25 12:27:12 +0100

    Can add bookmarks

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -2,18 +2,24 @@ require 'bookmark'
 
 describe Bookmark do
   describe '#self.all' do
-    it 'returns google' do
-      connection = PG.connect(dbname: 'bookmark_manager_test')
+    it 'all bookmarks' do
+      Bookmark.create(url: "http://www.makersacademy.com")
+      Bookmark.create(url: "http://www.destroyallsoftware.com")
+      Bookmark.create(url: "http://www.google.com")
 
-      # Add the test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
       bookmarks = Bookmark.all
-
+      
       expect(bookmarks).to include "http://www.makersacademy.com"
       expect(bookmarks).to include "http://www.destroyallsoftware.com"
       expect(bookmarks).to include "http://www.google.com"
+    end
+  end
+
+  describe '#self.create' do
+    it 'creats a new bookmark' do
+      Bookmark.create(url: 'http://www.test.com' )
+
+      expect(Bookmark.all).to include 'http://www.test.com'
     end
   end
 end
